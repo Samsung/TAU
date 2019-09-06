dac1f51b1dee62dc38dcfb5cc24ca73d7c993a9d
Pawel Kaczmarczyk <p.kaczmarczy@samsung.com>
2019-09-06 13:24:49 +0200

    TAU release 1.1.4
    Dedicated platform: Tizen 5.5
    
    New features in 1.1.0:
    
    - new widgets: Graph, Dimmer
    - Keyboard support
    - TV landscape layout
    
    Changes in version 1.1.4:
    
    - Utils: Added copy of attributes from original inline script to new one
    - TCT: RadioButton & Checkbox: fix for deprecated API synchronous XHTTPRequest
    - TCT: Change version of TCT to 5.5
    - Events/vmouse: method handleTouchMove doesn't ready for broken data
    - TCT: Add common files to tct test packages
    - Coverflow: Add the CoverFlow example for wearable profile
    - BaseWidget: fix for widget options getting from attributes
    - SectionChanger: fix for model/directives feature
    - SectionChanger: issue in test engine cause test fails
    - Graph: fix for color option split
    - DropDownMenu: update placeholder on widget refresh
    - Engine: add getBinding instance search on widget wrapper
    - Graph: add unit tests
    - Build: Add configurable bundled extensions build
    - FloatingActions: fix for button width
    - Graph: Fix graph plot color property
    - I3d: Update i3d sample source to use the gltf format
    - Listview: Fix failing mobile listview testcase
    - IndexScrollbar: Fix setting elements for indexscrollbar
    - BaseWidget: Add reading of widget properties during refresh
    - Selector: Extend Selector remove item API
    - TextInput: added distinguished class name for container in depend to input type
    - UIComponents: Drawer example in wearable UI Components
    
    Signed-off-by: Pawel Kaczmarczyk <p.kaczmarczy@samsung.com>

--- dist/mobile/js/tau.support-2.3.js
+++ dist/mobile/js/tau.support-2.3.js
@@ -4,7 +4,7 @@ var ns = window.tau = window.tau || {},
 nsConfig = window.tauConfig = window.tauConfig || {};
 nsConfig.rootNamespace = 'tau';
 nsConfig.fileName = 'tau';
-ns.version = '1.1.3';
+ns.version = '1.1.4';
 /*global window, ns, define */
 /*
  * Copyright (c) 2015 Samsung Electronics Co., Ltd
