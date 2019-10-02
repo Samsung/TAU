7220e1e1baf0ef328b70530543769c0cac25adab
Pawel Kaczmarczyk <p.kaczmarczy@samsung.com>
2019-10-02 13:11:42 +0200

    TAU release 1.0.11
    Dedicated platform: Tizen 4.0, Tizen 5.0
    
    New features in 1.0.0:
    
    - support for bezel-less devices
    - extended widget control by touch
    - new Date/Time pickers
    
    Changes in version 1.0.11:
    - (Mobile)(UIComponents) added dividers filtering for IndexScrollbar example
    - (Mobile)(SectionChanger) Make SectionChanger resizable
    - (Mobile)(Highlight) Fix for two items highlights in the same time
    - (Mobile)(Drawer) Fix for filling colored list in drawer widget
    - (Mobile)(Page) Storing and restoring page scroll position on page change
    - (Mobile)(UIComponents) Fix for duplicate page ID in bottom button example
    - (Wearable) Number, Time and Date pickers: remove "set" button on widget destroy
    - (Mobile) Expandalbe list: apply active state only for it's header
    - (Wearable) Change positioning of swipelist item text and icon
    - (Wearable) Fix selector icons disappearing
    - (Wearable)(UIComponents) Add missing btn-text class for button
    
    Signed-off-by: Pawel Kaczmarczyk <p.kaczmarczy@samsung.com>

--- dist/mobile/js/tau.support-2.3.js
+++ dist/mobile/js/tau.support-2.3.js
@@ -4,7 +4,7 @@ var ns = window.tau = window.tau || {},
 nsConfig = window.tauConfig = window.tauConfig || {};
 nsConfig.rootNamespace = 'tau';
 nsConfig.fileName = 'tau';
-ns.version = '1.0.10';
+ns.version = '1.0.11';
 /*global window, ns, define */
 /*
  * Copyright (c) 2015 Samsung Electronics Co., Ltd
