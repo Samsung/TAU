cc04cd0dc5cc6975955a98c45e3d930a1d32b01d
Tomasz Lukawski <t.lukawski@samsung.com>
2019-10-03 12:45:33 +0200

    TAU release 1.0.12
    Dedicated platform: Tizen 4.0, Tizen 5.0
    
    New features in 1.0.0:
    
    - support for bezel-less devices
    - extended widget control by touch
    - new Date/Time pickers
    
    Changes in version 1.0.12:
    - (util) globalize: block data loading when data has already exists
    
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- dist/wearable/js/tau.js
+++ dist/wearable/js/tau.js
@@ -20,7 +20,7 @@ var ns = window.tau = window.tau || {},
 nsConfig = window.tauConfig = window.tauConfig || {};
 nsConfig.rootNamespace = 'tau';
 nsConfig.fileName = 'tau';
-ns.version = '1.0.11';
+ns.version = '1.0.12';
 /*
  * Copyright (c) 2015 Samsung Electronics Co., Ltd
  *
