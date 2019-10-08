e14f388bf28f80375b5610d7f3668757be32c376
Pawel Kaczmarczyk <p.kaczmarczy@samsung.com>
2019-10-08 11:49:00 +0200

    TAU release 1.1.7
    Dedicated platform: Tizen 5.5
    
    New features in 1.1.0:
    
    - new widgets: Graph, Dimmer
    - Keyboard support
    - TV landscape layout
    
    Changes in version 1.1.7:
    
    - (wearable)(UIComponents) Fix for webkit date/time inputs appearance
    - (BaseWidget) Fix for converting number options during widget refresh
    - (wearable)(UIComponents) fix for header height in DatePicker sample
    
    Signed-off-by: Pawel Kaczmarczyk <p.kaczmarczy@samsung.com>

--- dist/mobile/js/tau.js
+++ dist/mobile/js/tau.js
@@ -20,7 +20,7 @@ var ns = window.tau = window.tau || {},
 nsConfig = window.tauConfig = window.tauConfig || {};
 nsConfig.rootNamespace = 'tau';
 nsConfig.fileName = 'tau';
-ns.version = '1.1.6';
+ns.version = '1.1.7';
 /*
  * Copyright (c) 2015 Samsung Electronics Co., Ltd
  *
