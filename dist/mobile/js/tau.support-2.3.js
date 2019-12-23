3da63f784b85fcc95d8c0815022605eb5abce7d5
Lukasz Slachciak <l.slachciak@samsung.com>
2019-12-23 13:41:57 +0100

    TAU 1.1.13 release
    Dedicated platform: Tizen 5.5
    
    New features in 1.1:
     - new widgets: Graph, Dimmer
     - Keyboard support
     - TV landscape layout
    
    Commits in version 1.1.13:
    
    6377e97 Merge remote-tracking branch 'origin/tau_1.0' into HEAD
    fe7da93 (origin/tau_1.0, origin/HEAD) Listview: set background color for not colored listview
    caff9e1 (example) SinglePage app: change styles specific for app
    d558feb Marquee: fix to show left gradient in Marquee List (#518)
    84be2f4 (tag: v1.0.21) TAU release 1.0.21
    8f42b62 Fix the Popup Footer button (#512)
    404ba19 (origin/tau_1.1) Fix problem that scrollToPosition is not working correctly (#511)
    98eab68 Fix problem that scrollToPosition is not working correctly (#509)
    8fc3ff7 (example) ArcListview: fix for issue with broken list after back to the page
    6f3f06c (tag: v1.0.20) TAU release 1.0.20
    b354bda SnapListview: fix for detect scrollend
    246fa65 Bottom button highlights out of area when clicked (#506)
    3894687 Bottom button highlights out of area when clicked (#505)
    
    Signed-off-by: Lukasz Slachciak <l.slachciak@samsung.com>

--- dist/mobile/js/tau.support-2.3.js
+++ dist/mobile/js/tau.support-2.3.js
@@ -4,7 +4,7 @@ var ns = window.tau = window.tau || {},
 nsConfig = window.tauConfig = window.tauConfig || {};
 nsConfig.rootNamespace = 'tau';
 nsConfig.fileName = 'tau';
-ns.version = '1.1.12';
+ns.version = '1.1.13';
 /*global window, ns, define */
 /*
  * Copyright (c) 2015 Samsung Electronics Co., Ltd
--- dist/tv/js/tau.js
+++ dist/tv/js/tau.js
@@ -20,7 +20,7 @@ var ns = window.tau = window.tau || {},
 nsConfig = window.tauConfig = window.tauConfig || {};
 nsConfig.rootNamespace = 'tau';
 nsConfig.fileName = 'tau';
-ns.version = '1.1.12';
+ns.version = '1.1.13';
 /*
  * Copyright (c) 2015 Samsung Electronics Co., Ltd
  *
