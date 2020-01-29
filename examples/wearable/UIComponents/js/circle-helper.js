Samsung/TAU

commit 9f972cedc218c1847b4705e1f1c57f0a73a05569

(UIComponents) SnapListView: fix for wrong selection on rotary scrolling
[Issue] https://github.com/Samsung/TAU/issues/561
[Problem] SnapListView + rotary scrolling - wrong selection position
[Solution] The Snaplistview example has two listeners on "pagebeforeshow".
Listener from circular-helper was called after listener from snaplistview.js.
This cause issue with locking of rotaryScroll when rotary scroll was not
enabled yet.

Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

examples/wearable/UIComponents/js/circle-helper.js
@@ -32,7 +32,7 @@ document.addEventListener("tauinit", function () {
 					tau.widget.Listview(list);
 				}
 			}
-		});
+		}, true);
 		document.addEventListener("pagebeforehide", function (event) {
 			var page = event.target,
 				pageWidget = tau.widget.Page(page);

commit 6ee30977a35da46f15cf1339fd4b5ab5a8aa6409

fixup! (UIComponents) SnapListView: fix for wrong selection on rotary scrolling
[Issue] https://github.com/Samsung/TAU/issues/569
[Problem] Drawer example shows list items on the start
[Solution] Avoid list creation on drawer initialization.
This is fix for regression after 9f972ce

Signed-off-by: Lukasz Slachciak <l.slachciak@samsung.com>

examples/wearable/UIComponents/js/circle-helper.js
@@ -26,7 +26,8 @@ document.addEventListener("tauinit", function () {
 				pageId !== "page-snaplistview" &&
 				pageId !== "page-swipelist" &&
 				pageId !== "page-marquee-list" &&
-				pageId !== "page-multiline-list") {
+				pageId !== "page-multiline-list" &&
+				pageId !== "drawer-page") {
 				list = page.querySelector(".ui-listview");
 				if (list) {
 					tau.widget.Listview(list);