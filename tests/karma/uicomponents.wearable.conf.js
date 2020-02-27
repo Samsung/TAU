249e4fe564fdd874415f39e4a91d4a63cc20ec5e
Lukasz Slachciak <l.slachciak@samsung.com>
2020-02-27 13:06:55 +0100

    Fix eslint problem
    [Issue] N/A
    [Problem] Eslint error on travis
    [Solution] Fix identation in file
    
    Signed-off-by: Lukasz Slachciak <l.slachciak@samsung.com>

--- tests/karma/uicomponents.wearable.conf.js
+++ tests/karma/uicomponents.wearable.conf.js
@@ -4,11 +4,11 @@
 module.exports = function (config) {
 	config.set({
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
+		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: "../../",
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: ["requirejs", "qunit"],
 
 		reporters: ["progress", "html", "console", "coverage"],
@@ -23,51 +23,51 @@ module.exports = function (config) {
 			"karma-qunit",
 			"karma-coverage",
 			"karma-html-reporter",
-            // This is ours custom module,
-            // To install, run
-            //      npm install tools/karma-plugins/console-reporter/
-            //      sudo npm install tools/karma-plugins/console-reporter/ -g
+			// This is ours custom module,
+			// To install, run
+			//      npm install tools/karma-plugins/console-reporter/
+			//      sudo npm install tools/karma-plugins/console-reporter/ -g
 			"karma-console-reporter"
 		],
 		autoWatch: true,
-        // list of files / patterns to load in the browser
+		// list of files / patterns to load in the browser
 		files: [
-            {pattern: "tests/libs/qunit-1.11.0.js", included: true, served: true},
-            {pattern: "tests/libs/jquery.js", included: true, served: true},
-            {pattern: "libs/path-to-regexp.js", included: false, served: true},
-            {pattern: "tests/libs/require.js", included: false, served: true},
+			{pattern: "tests/libs/qunit-1.11.0.js", included: true, served: true},
+			{pattern: "tests/libs/jquery.js", included: true, served: true},
+			{pattern: "libs/path-to-regexp.js", included: false, served: true},
+			{pattern: "tests/libs/require.js", included: false, served: true},
 
-            {pattern: "examples/wearable/UIComponents/**/*", included: false, served: true},
-            // // {pattern: "examples/wearable/UIComponentsCE/**/*", included: false, served: true},
+			{pattern: "examples/wearable/UIComponents/**/*", included: false, served: true},
+			// // {pattern: "examples/wearable/UIComponentsCE/**/*", included: false, served: true},
 
-            {pattern: "tests/karma/tests/helpers.js", included: false, served: true, watch: true},
-            {pattern: "tests/karma/tests/compare-helper.js", included: false, served: true, watch: true},
-            {pattern: "tests/karma/tests/compare-helper-excludes.js", included: false, served: true, watch: true},
-            {pattern: "tests/karma/tests/properties-typeof-compare.js", included: false, served: true, watch: true},
+			{pattern: "tests/karma/tests/helpers.js", included: false, served: true, watch: true},
+			{pattern: "tests/karma/tests/compare-helper.js", included: false, served: true, watch: true},
+			{pattern: "tests/karma/tests/compare-helper-excludes.js", included: false, served: true, watch: true},
+			{pattern: "tests/karma/tests/properties-typeof-compare.js", included: false, served: true, watch: true},
 
 			// here put path to tests
 
-            {pattern: "tests/karma/tests/**/wearable-test.js", included: false, served: true, watch: true},
+			{pattern: "tests/karma/tests/**/wearable-test.js", included: false, served: true, watch: true},
 			"tests/karma/testPaths.js",
 			"tests/karma/runner.js"
 		],
 
 
-        // web server port
+		// web server port
 		port: 9876,
 
 
-        // enable / disable colors in the output (reporters and logs)
+		// enable / disable colors in the output (reporters and logs)
 		colors: true,
 
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
 
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		customLaunchers: {
 			bigScreen: {
 				base: "Chrome",
@@ -78,8 +78,8 @@ module.exports = function (config) {
 		browsers: ["bigScreen"],
 		// browsers: ["ChromeHeadless"],
 
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
 		singleRun: false,
 
 		htmlReporter: {
