fb0694cc767b894c91691cd9617d68b63ea47c7d
Tomasz Lukawski <t.lukawski@samsung.com>
2020-03-05 13:50:15 +0100

    (wearable) AssistPanel: increasing tests coverage
    [Issue] https://github.com/Samsung/TAU/issues/723
    [Problem] Assist Panel has not tests
    [Solution] Added tests to Assist Panel
    
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- tests/karma/single.test.path.js
+++ tests/karma/single.test.path.js
@@ -1,4 +1,7 @@
 /*global testPaths:true */
 testPaths = [
-	"core/widget/core/VirtualListviewSimple"
+	"profile/wearable/widget/wearable/AssistPanel"
 ];
+
+// For test:
+// > ./node_modules/karma/bin/karma start tests/karma/single.conf.js
