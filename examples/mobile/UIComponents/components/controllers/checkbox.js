a61a3012f3b38bad57c271a40969bf7a1e08d581
Lukasz Slachciak <l.slachciak@samsung.com>
2020-03-11 12:52:03 +0100

    Checkbox (List) - update example
    [Issue] https://github.com/Samsung/TAU/issues/720
    [Problem] Not up-to-date example
    [Solution] Rewrite example
    
    Signed-off-by: Lukasz Slachciak <l.slachciak@samsung.com>

--- examples/mobile/UIComponents/components/controllers/checkbox.js
+++ examples/mobile/UIComponents/components/controllers/checkbox.js
@@ -7,27 +7,40 @@
 	 * favoriteResult - Indicator for the favorite checkbox state
 	 */
 	var page = document.getElementById("checkbox-demo"),
-		checkone = document.getElementById("checkbox-1"),
-		checkboxResult = document.querySelector(".checkbox-result"),
-		favortieone = document.getElementById("favorite-1"),
-		favoriteResult = document.querySelector(".favorite-result");
+		checkboxes = document.querySelectorAll(".checkboxes"),
+		idx,
+		checkboxText1 = document.getElementById("checkbox-text-1"),
+		checkboxText2 = document.getElementById("checkbox-text-2");
+
+	/**
+	 * Updates text for an selected radio
+	 * @param {HTMLElement} target
+	 */
+	function setCheckboxResultFromTarget(target) {
+		var result;
+
+		if (target.checked) {
+			result = "Check on";
+		} else {
+			result = "Check off"
+		}
+
+		if (target.name == "checkbox-1") {
+			checkboxText1.innerHTML = result;
+		} else if (target.name == "checkbox-2") {
+			checkboxText2.innerHTML = result;
+		}
+	}
 
 	/**
 	 * pageshow event handler
 	 * Do preparatory works and adds event listeners
 	 */
 	page.addEventListener("pageshow", function () {
-
-		checkone.addEventListener("click", function () {
-			var value = checkone.checked;
-
-			checkboxResult.innerHTML = "The First Checkbox is " + value;
-		});
-
-		favortieone.addEventListener("click", function () {
-			var value = favortieone.checked;
-
-			favoriteResult.innerHTML = "The First Favorite is " + value;
-		});
+		for (idx = 0; idx < checkboxes.length; idx++) {
+			checkboxes[idx].addEventListener("change", function (event) {
+				setCheckboxResultFromTarget(event.target);
+			})
+		}
 	});
 }());
