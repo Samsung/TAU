fbdad7842109ac3b4c2161cb4136d616b47bde12
DohyungLim <delight.lim@samsung.com>
2020-04-21 17:40:22 +0900

    [mobile] Add progressbar to the dialog sample
    - Add progressbar dialog menu.
    - Add sample function to move progressbar in dialog.js .

--- examples/mobile/UIComponents/components/dialogs/dialog.js
+++ examples/mobile/UIComponents/components/dialogs/dialog.js
@@ -1,6 +1,11 @@
 (function (document, tau) {
 	var page = document.getElementById("demo-dialog-page"),
 		buttons = document.querySelectorAll(".ui-btn"),
+		progressPage = page.querySelector("#progressbar-dialog"),
+		progressbar = page.querySelector("#progressbar"),
+		progressPercent = page.querySelector("#percent"),
+		setTimer,
+		getTimer,
 		idx;
 
 	function onClick() {
@@ -11,12 +16,31 @@
 		for (idx = 0; idx < buttons.length; idx++) {
 			buttons[idx].addEventListener("vclick", onClick);
 		}
+		progressPage.addEventListener("popupshow", onProgressPageShow);
 	}
 
 	function onPageHide() {
 		for (idx = 0; idx < buttons.length; idx++) {
 			buttons[idx].removeEventListener("vclick", onClick);
 		}
+		progressPage.addEventListener("popuphide", onProgressPageHide);
+	}
+
+	function onProgressPageShow() {
+		var progressbarWidget = new tau.widget.Progress(progressbar);
+
+		setTimer = setTimeout(function () {
+			progressbarWidget.value(100);
+		}, 1000);
+
+		getTimer = setTimeout(function () {
+			progressPercent.innerHTML = progressbarWidget.value() + "%";
+		}, 2000);
+	}
+
+	function onProgressPageHide() {
+		clearTimeout(setTimer);
+		clearTimeout(getTimer);
 	}
 
 	page.addEventListener("pagebeforeshow", onPageShow);
