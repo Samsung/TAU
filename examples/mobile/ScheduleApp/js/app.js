ece2030862daf705966f2bb828246bb865fffaad
Tomasz Lukawski <t.lukawski@samsung.com>
2020-05-21 15:58:39 +0200

    (mobile) ScheduleApp: Change DatePicker to Calendar on date click
    [Issue] https://github.com/Samsung/TAU/issues/1125
    [Problem] Clicking on date in Date & Time picker should display Date component
    [Solution]
     The patch implement this feature in the app but this functionality
     will become part of the TAU
    
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- examples/mobile/ScheduleApp/js/app.js
+++ examples/mobile/ScheduleApp/js/app.js
@@ -1,4 +1,6 @@
 (function () {
+	var tau = window.tau;
+
 	window.addEventListener("tizenhwkey", function (ev) {
 		var activePopup = null,
 			page = null,
@@ -19,4 +21,43 @@
 			}
 		}
 	});
+
+	document.addEventListener("pagebeforeshow", function (event) {
+		var page = event.target;
+
+		if (page.id === "create-event-page") {
+			page.addEventListener("selected", function (event) {
+				var calendarEl,
+					calendar;
+
+				if (event.target.id === "date-on") {
+					calendarEl = document.getElementById("calendar-on");
+				} else if (event.target.id === "date-off") {
+					calendarEl = document.getElementById("calendar-off");
+				}
+				if (calendarEl) {
+					calendar = tau.widget.Calendar(calendarEl);
+					calendar.value(event.detail.datetime);
+					calendar.element.classList.remove("app-hidden");
+					event.target.classList.add("app-hidden");
+				}
+			});
+			page.addEventListener("calendarswitch", function (event) {
+				var dateEl,
+					date;
+
+				if (event.target.id === "calendar-on") {
+					dateEl = document.getElementById("date-on");
+				} else if (event.target.id === "calendar-off") {
+					dateEl = document.getElementById("date-off");
+				}
+				if (dateEl) {
+					date = tau.widget.DateTimePicker(dateEl);
+					date.value(event.detail.date);
+					date.element.classList.remove("app-hidden");
+					event.target.classList.add("app-hidden");
+				}
+			});
+		}
+	}, true);
 }());
