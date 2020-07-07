7490b2ccb2a7f47d7d2b5e5d3c280c2a65a6da0d
Kornelia Kobiela <korneliak.95@gmail.com>
2020-07-07 16:05:36 +0200

    [HomeTvApp] Add widget dinamically
    [Issue] #1270
    [Problem] All widgets are static items
    [Solution] Creating popup to add widgets dinamically
    
    Signed-off-by: Kornelia Kobiela <korneliak.95@gmail.com>

--- examples/mobile/HomeApp/js/app.js
+++ examples/mobile/HomeApp/js/app.js
@@ -1,9 +1,37 @@
 (function () {
 	"use strict";
-	var tau = window.tau;
+	var tau = window.tau,
+		state = {
+			widgets: {
+				news: false,
+				nowontv: false
+			}
+		};
+
+	function getWidgetsState() {
+		var latestNews = document.querySelector("#latest-news-check"),
+			nowOnTV = document.querySelector("#now-on-tv-check");
+
+		state.widgets.news = latestNews.checked;
+		state.widgets.nowontv = nowOnTV.checked;
+	}
+
+	function toggleWidgetsState() {
+		var latestNews = document.querySelector("#latest-news-container"),
+			nowOnTv = document.querySelector("#now-on-tv-container");
+
+		latestNews.classList.toggle("app-display-none", !state.widgets.news);
+		nowOnTv.classList.toggle("app-display-none", !state.widgets.nowontv);
+	}
 
 	function changeTheme(event) {
 		tau.theme.setTheme(event.target.value);
+	}
+
+	function onPopupSubmit() {
+		getWidgetsState();
+		toggleWidgetsState();
+		tau.history.back();
 	}
 
 	/**
@@ -20,7 +48,8 @@
 		var themeChanger = document.querySelector("#theme-selector"),
 			page = document.querySelector(".ui-page"),
 			themeChangerButton = page.querySelector("#selector-opener"),
-			burgerButton = page.querySelector(".app-btn-icon-burger");
+			burgerButton = page.querySelector(".app-btn-icon-burger"),
+			popupButton = page.querySelector("#popup-submit");
 
 		themeChanger.addEventListener("change", changeTheme);
 
@@ -30,7 +59,8 @@
 			dropdownmenuWidget.open();
 		});
 
-		burgerButton.addEventListener("click", onButtonClick)
+		burgerButton.addEventListener("click", onButtonClick);
+		popupButton.addEventListener("click", onPopupSubmit);
 	}
 
 	document.addEventListener("pagebeforeshow", init);
