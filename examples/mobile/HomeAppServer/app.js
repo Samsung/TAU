1dff9ad93b11cbbe9a94e4ea405bd2f55edc9703
Lukasz Slachciak <l.slachciak@samsung.com>
2020-08-12 15:30:06 +0200

    Provide API for getting webclip list
    [Issue] https://github.com/Samsung/TAU/issues/1365
    [Problem] No API to get list of webclips
    [Solution] Provide API. Remove unneeded files
    
    Signed-off-by: Lukasz Slachciak <l.slachciak@samsung.com>

--- examples/mobile/HomeAppServer/app.js
+++ examples/mobile/HomeAppServer/app.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const fs = require("fs"),
+	homeAppPath = "../HomeApp",
+	webClipDir = "webclip";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan"),
+	app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../HomeApp')));
+app.use("/", express.static(path.join(__dirname, homeAppPath)));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/api/webcliplist", (req, res) => {
+	fs.readdir(homeAppPath + "/" + webClipDir, (err, files) => {
+		var result = [];
+
+		if (!err) {
+			files.forEach((file) => {
+				result.push(webClipDir + "/" + file)
+			});
+		}
+		res.header("Content-Type", "application/json");
+		res.send(JSON.stringify(result));
+	});
+})
+
 
 module.exports = app;
