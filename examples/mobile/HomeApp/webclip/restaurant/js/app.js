ae397c15ad2b743b36d66b5793301dead73434a1
Tomasz Lukawski <t.lukawski@samsung.com>
2020-09-16 14:55:37 +0200

    Card: load css styles by XMLHttpRequest
    [Issue] https://github.com/Samsung/TAU/issues/1395
    [Problem] Section Changer require refresh method on time out
    [Solution]
     - webClip css files need load before widget create
    
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- examples/mobile/HomeApp/webclip/restaurant/js/app.js
+++
@@ -1,9 +0,0 @@
-var sectionChanger = document.querySelector(".ui-section-changer"),
-	tau = window.tau;
-
-tau.engine.createWidgets(sectionChanger);
-
-// refresh section changer
-window.setTimeout(function () {
-	tau.engine.getBinding(sectionChanger).refresh();
-}, 200);
