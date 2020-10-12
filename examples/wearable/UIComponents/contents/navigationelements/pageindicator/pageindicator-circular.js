35ac9270c9fc58236828c48676039e6bc779685e
Hunseop Jeong <hs85.jeong@samsung.com>
2020-10-12 10:29:31 +0900

    Change the example to fit the content
    [Issue] N/A
    [Problem] The name of "Circularly horizontal dot" and the example
              content do not match
    [Solution] Modify the example to match with the name
    
    Signed-off-by: Hunseop Jeong <hs85.jeong@samsung.com>

---
+++ examples/wearable/UIComponents/contents/navigationelements/pageindicator/pageindicator-circular.js
@@ -0,0 +1,48 @@
+/*global tau */
+(function () {
+
+	var page = document.getElementById("pageIndicatorCircularPage"),
+		changer = document.getElementById("hsectionchanger"),
+		sections = document.querySelectorAll("section"),
+		sectionChanger,
+		elPageIndicator = document.getElementById("pageIndicator"),
+		pageIndicator,
+		pageIndicatorHandler;
+
+	/**
+	 * pagebeforeshow event handler
+	 * Do preparatory works and adds event listeners
+	 */
+	page.addEventListener("pagebeforeshow", function () {
+		// make PageIndicator
+		pageIndicator = tau.widget.PageIndicator(elPageIndicator, {numberOfPages: sections.length});
+		pageIndicator.setActive(0);
+		// make SectionChanger object
+		sectionChanger = new tau.widget.SectionChanger(changer, {
+			circular: true,
+			orientation: "horizontal",
+			useBouncingEffect: true
+		});
+	});
+
+	/**
+	 * pagehide event handler
+	 * Destroys and removes event listeners
+	 */
+	page.addEventListener("pagehide", function () {
+		// release object
+		sectionChanger.destroy();
+		pageIndicator.destroy();
+	});
+
+	/**
+	 * sectionchange event handler
+	 * @param {Event} e
+	 */
+	pageIndicatorHandler = function (e) {
+		pageIndicator.setActive(e.detail.active);
+	};
+
+	changer.addEventListener("sectionchange", pageIndicatorHandler, false);
+
+}());
