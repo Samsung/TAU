907fee7ce66fae3fd86cf55a5749d86900eff423
Lukasz Slachciak <l.slachciak@samsung.com>
2020-10-13 15:58:57 +0200

    [HomeApp] Add ads card example.
    [Issue] https://github.com/Samsung/TAU/issues/1448
    [Problem] No ads card example in sample app
    [Solution] Added ads card example
    
    Signed-off-by: Lukasz Slachciak <l.slachciak@samsung.com>

--- examples/mobile/HomeAppServer/app.js
+++ examples/mobile/HomeAppServer/app.js
@@ -26,6 +26,17 @@ var logger = require("morgan"),
 		]
 	},
 	{
+		"appID": "vUf39tzQ4s.UIComponents",
+		"isInstalled": false,
+		"isActive": false,
+		"webClipsList": [
+			{
+				url: "webclip/netflix",
+				isSelected: true
+			}
+		]
+	},
+	{
 		"appID": "vUf39tzQ3t.UIComponents",
 		"isInstalled": true,
 		"isActive": false,
