05ff84cef517eb4932540b04173470fcddfb384d
Tomasz Lukawski <t.lukawski@samsung.com>
2021-03-08 16:18:34 +0100

    Search: Auto complete sugesstions
    [Issue] https://github.com/Samsung/TAU/issues/1605
    [Problem] auto complete suggestion - template not implemented
    [Solution]
     - added new template
     - added new css style
    
    Signed-off-by: Tomasz Lukawski <t.lukawski@samsung.com>

--- examples/mobile/UIComponents/components/search/search-bar-auto-complete.js
+++ examples/mobile/UIComponents/components/search/search-bar-auto-complete.js
@@ -1,6 +1,7 @@
 (function (document, tau) {
-	var page = document.getElementById("search-bar-page"),
-		search;
+	var page = document.getElementById("search-bar-auto-complete-page"),
+		search,
+		list;
 
 	function onSearchFocus() {
 		var popupElement = page.querySelector(".ui-popup");
@@ -10,15 +11,26 @@
 		}
 	}
 
+	function onSuggestionClick(ev) {
+		var text = tau.util.selectors.getClosestBySelector(ev.target, "li").textContent.trim();
+
+		search.value = text;
+	}
+
 	function onPageShow() {
 		search = page.querySelector("header .ui-appbar-title-container input[type='search']");
 		search.addEventListener("focus", onSearchFocus);
+
+		list = page.querySelector(".ui-listview-search-auto-complete");
+		list.addEventListener("vclick", onSuggestionClick, true);
 	}
 
 	function onPageHide(ev) {
 		var page = ev.target;
 
 		search.removeEventListener("focus", onSearchFocus);
+		list.removeEventListener("vclick", onSuggestionClick, true);
+
 		page.removeEventListener("pageshow", onPageShow);
 		page.removeEventListener("pagehide", onPageHide);
 	}
