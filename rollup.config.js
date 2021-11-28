f3a1e87a46345bb05808be414a43c84278da730d
Hunseop Jeong <hs85.jeong@samsung.com>
2021-11-28 12:20:18 +0900

    Add the index page for examples
    Signed-off-by: Hunseop Jeong <hs85.jeong@samsung.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,18 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import serve from 'rollup-plugin-serve';
 import pkg from './package.json';
 
 const extensions = ['.js', '.ts'];
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: 'src/one-elements.ts',
   plugins: [
     resolve({ extensions }),
-    typescript()
+    typescript(),
+    !production && serve({open: true, openPage: '/examples/index.html', contentBase: ['examples'], port: 8080})
   ],
   output: [
     {
