67a90729152b115a82495b2c799f69019c717cd0
Hunseop Jeong <hs85.jeong@samsung.com>
2021-11-28 16:40:40 +0900

    Add the disabled property and test
    Signed-off-by: Hunseop Jeong <hs85.jeong@samsung.com>

--- test/one-button.js
+++ test/one-button.js
@@ -16,6 +16,42 @@ describe('One-Button', () => {
       container.html(html);
       const element = container.find('one-button');
       expect(element.text()).to.eql('Test');
-    })
+    });
+  });
+
+  describe('Property check', () => {
+    it('focus', async () => {
+      const html = '<one-button id="btn">Focus</one-button>';
+      const container = await cy.get('#container');
+      container.html(html);
+
+      // Wait for inserting the shadow element of the one-button
+      await cy.wait(500);
+
+      const oneButton = win.document.body.querySelector('#btn');
+      const button = oneButton.shadowRoot.querySelector('button');
+      const path = oneButton.shadowRoot.querySelector('path');
+      button.focus();
+
+      const styles = win.getComputedStyle(path);
+      expect(styles.strokeWidth).to.eql('1.5px');
+    });
+
+    it('disabled', async () => {
+      const html = '<one-button id="btn" disabled>Disabled</one-button>';
+      const container = await cy.get('#container');
+      container.html(html);
+
+      // Wait for inserting the shadow element of the one-button
+      await cy.wait(500);
+
+      const oneButton = win.document.body.querySelector('#btn');
+      const button = oneButton.shadowRoot.querySelector('button');
+      expect(button.hasAttribute('disabled')).to.be.ok;
+      const styles = win.getComputedStyle(button);
+      expect(styles.opacity).to.eql('0.6');
+      expect(styles.cursor).to.eql('default');
+      expect(styles.pointerEvents).to.eql('none');
+    });
   });
 });
