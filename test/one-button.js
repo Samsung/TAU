0a9b540523f122b0bd15db696452ddd05dd1d41e
Hunseop Jeong <hs85jeong@gmail.com>
2022-06-19 14:57:06 +0900

    Update the one-button and the one-checkbox test
    - Add properties test for the color and the size to the one-button
    - Add the interaction test for the one-checkbox to check whether
      the checked value is changed correctly
    
    Signed-off-by: Hunseop Jeong <hs85jeong@gmail.com>

--- test/one-button.js
+++ test/one-button.js
@@ -23,14 +23,14 @@ describe('One-Button', () => {
 
   describe('Property check', () => {
     it('focus', async () => {
-      const html = '<one-button id="btn">Focus</one-button>';
+      const html = '<one-button>Focus</one-button>';
       const container = await cy.get('#container');
       container.html(html);
 
       // Wait for inserting the shadow element of the one-button
       await cy.wait(500);
 
-      const oneButton = win.document.body.querySelector('#btn');
+      const oneButton = win.document.body.querySelector('one-button');
       const button = oneButton.shadowRoot.querySelector('button');
       const path = oneButton.shadowRoot.querySelector('path');
       button.focus();
@@ -40,20 +40,46 @@ describe('One-Button', () => {
     });
 
     it('disabled', async () => {
-      const html = '<one-button id="btn" disabled>Disabled</one-button>';
+      const html = '<one-button disabled>Disabled</one-button>';
       const container = await cy.get('#container');
       container.html(html);
 
       // Wait for inserting the shadow element of the one-button
       await cy.wait(500);
 
-      const oneButton = win.document.body.querySelector('#btn');
+      const oneButton = win.document.body.querySelector('one-button');
       const button = oneButton.shadowRoot.querySelector('button');
       expect(button.hasAttribute('disabled')).to.be.ok;
       const styles = win.getComputedStyle(button);
       expect(styles.opacity).to.eql('0.6');
       expect(styles.cursor).to.eql('default');
       expect(styles.pointerEvents).to.eql('none');
+    });
+
+    it('color', async () => {
+      const html = '<one-button color="red">Red</one-button>';
+      const container = await cy.get('#container');
+      container.html(html);
+
+      // Wait for inserting the shadow element of the one-button
+      await cy.wait(500);
+
+      const oneButton = win.document.body.querySelector('one-button');
+      const button = oneButton.shadowRoot.querySelector('button');
+      expect(button.hasAttribute('color')).to.eql(true);
+    });
+
+    it('size', async () => {
+      const html = '<one-button size="small">Small</one-button>';
+      const container = await cy.get('#container');
+      container.html(html);
+
+      // Wait for inserting the shadow element of the one-button
+      await cy.wait(500);
+
+      const oneButton = win.document.body.querySelector('one-button');
+      const button = oneButton.shadowRoot.querySelector('button');
+      expect(button.hasAttribute('size')).to.eql(true);
     });
   });
 });
--- test/one-checkbox.js
+++ test/one-checkbox.js
@@ -2,7 +2,7 @@ const LOCAL_HOST = 'http://localhost:9000';
 
 describe('One-Checkbox', () => {
   let win = null;
-  before(async () => {
+  beforeEach(async () => {
     win = await cy.visit(`${LOCAL_HOST}/test/fixtures/test.html`);
   });
 
@@ -24,6 +24,53 @@ describe('One-Checkbox', () => {
 
       const element = container.find('one-checkbox');
       expect(element.text()).to.eql('Test');
+    });
+  });
+
+  describe('Property check', () => {
+    it('default checked value', async () => {
+      const html = '<one-checkbox>Test</one-checkbox>';
+      const container = await cy.get('one-circle');
+      container.html(html);
+
+      const element = container.find('one-checkbox');
+      expect(element.prop('checked')).to.eql(false);
+    });
+
+    it('checked', async () => {
+      const html = '<one-checkbox checked>Test</one-checkbox>';
+      const container = await cy.get('one-circle');
+      container.html(html);
+
+      const element = container.find('one-checkbox');
+      expect(element.prop('checked')).to.eql(true);
+    });
+  });
+
+  describe('Interaction', () => {
+    it('Click the checkbox', async () => {
+      const html = '<one-checkbox>Test</one-checkbox>';
+      const container = win.document.querySelector('one-circle');
+      container.innerHTML = html;
+
+      const oneCheckbox = container.querySelector('one-checkbox');
+      expect(oneCheckbox.hasAttribute('checked')).to.eql(false);
+
+      // Wait for inserting the shadow element of the one-button
+      await cy.wait(100);
+
+      const input = oneCheckbox.shadowRoot.querySelector('input');
+      input.click();
+
+      // Wait for changing element properties
+      await cy.wait(100);
+      expect(oneCheckbox.hasAttribute('checked')).to.eql(true);
+
+      input.click();
+
+      // Wait for changing element properties
+      await cy.wait(100);
+      expect(oneCheckbox.hasAttribute('checked')).to.eql(false);
     });
   });
 });
