a0281026d0389d7176f18a0c976c948888e1a236
Hunseop Jeong <hs85.jeong@samsung.com>
2022-07-21 20:41:43 +0900

    Add the disabled property for the one-checkbox
    - Extend the LitElement instead of the OneBase
    
    Signed-off-by: Hunseop Jeong <hs85.jeong@samsung.com>

--- src/one-checkbox.ts
+++ src/one-checkbox.ts
@@ -1,11 +1,12 @@
-import { css, CSSResultArray, html, TemplateResult } from 'lit';
+import { css, CSSResultArray, html, LitElement, PropertyValues, TemplateResult } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
-import { BaseCSS, OneBase, Point } from './one-base'
+import { BaseCSS, fireEvent } from './one-base'
 import { line, rectangle, svgNode } from './one-lib';
 
 @customElement('one-checkbox')
-export class OneCheckbox extends OneBase {
+export class OneCheckbox extends LitElement {
   @property({ type: Boolean, reflect: true }) checked = false;
+  @property({ type: Boolean, reflect: true }) disabled = false;
 
   @query('input') private input?: HTMLInputElement;
 
@@ -18,6 +19,12 @@ export class OneCheckbox extends OneBase {
       :host {
         display: inline-block;
         font-family: inherit;
+        opacity: 0;
+      }
+      :host(.one-disabled) {
+        opacity: 0.6 !important;
+        cursor: default;
+        pointer-events: none;
       }
       #container {
         display: flex;
@@ -44,45 +51,56 @@ export class OneCheckbox extends OneBase {
     `];
   }
 
-  oneRender(force: boolean = false) {
-    super.oneRender(force);
-  }
-
   render(): TemplateResult {
     return html`
-    <lablel id="container">
+    <label id="container">
       <input type="checkbox"
         .checked="${this.checked}"
         @change="${this.onChange}"
       >
       <span><slot></slot></span>
-      <div id="overlay"><svg></svg></div>
-    </lablel>
+      <div id="overlay"><svg id="svg"></svg></div>
+    </label>
     `;
   }
 
-  protected canvasSize(): Point {
-    return [24, 24];
-  }
+  updated(changed: PropertyValues) {
+    if (changed.has('disabled')) {
+      this.refreshDisabledState();
+    }
 
-  protected draw(svg: SVGSVGElement, size: Point) {
+    const svg = (this.shadowRoot!.getElementById('svg') as any) as SVGSVGElement;
+    while (svg.hasChildNodes()) {
+      svg.removeChild(svg.lastChild!);
+    }
+
+    const size = [24, 24];
     rectangle(svg, 0, 0, size[0], size[1]);
     this.svgCheck = svgNode('g');
     svg.appendChild(this.svgCheck);
     line(this.svgCheck, size[0] * 0.3, size[1] * 0.4, size[0] * 0.5, size[1] * 0.7)
     line(this.svgCheck, size[0] * 0.5, size[1] * 0.7, size[0] + 5, -5);
     this.refreshCheckVisibility();
+    this.classList.add('one-rendered');
   }
 
   private onChange() {
     this.checked = this.input!.checked;
     this.refreshCheckVisibility();
-    this.fire('change', { checked: this.checked });
+    fireEvent(this, 'change', { checked: this.checked });
   }
 
   private refreshCheckVisibility() {
     if (this.svgCheck) {
       this.svgCheck.style.display = this.checked ? '' : 'none';
+    }
+  }
+
+  private refreshDisabledState() {
+    if (this.disabled) {
+      this.classList.add('one-disabled');
+    } else {
+      this.classList.remove('one-disabled');
     }
   }
 }
